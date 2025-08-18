8fc2116bc1478b7422b36c1934041b36d074b95d
petsereypanha <sereypanhapet@gmail.com>
2025-08-18 23:19:03 +0700

    add Introduction to Matplotlib Data Visualization

--- clean/print.py
+++ clean/print.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
